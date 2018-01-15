12030f9ed8648d0b42652590c002f1c2118f8e59
andyqier88 <git@github.com:andyqier88/koa-blog.git>
2018-01-15 17:34:32 +0800

    前端部分

---
+++ src/myblog/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png"> -->
+    <header-div :logso = "logoMsg"></header-div>
+    <hello-world>{{logoMsg}}</hello-world>
+  </div>
+</template>
+
+<script>
+/* eslint-disable */
+import HeaderDiv from './components/HeaderDiv'
+import HelloWorld from './components/HelloWorld'
+export default {
+  name: 'App',
+  data(){
+    return {
+      logoMsg:'test'
+    }
+  },
+  components:{
+    HeaderDiv,
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/myblog/src/components/HeaderDiv.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="header">
+    <span>I am {{logso}}</span>
+  </div>
+</template>
+
+<script>
+/* eslint-disable */
+export default {
+  name: 'HeaderDiv',
+  data(){
+    return {
+      navs:[
+        {li:'主页'},
+        {li:'日志'},
+        {li:'说说'},
+        {li:'主页'},
+        {li:'主页'}
+      ]
+    }
+  },
+  props:['logso']
+}
+</script>
+
+<style>
+  .header{
+    color: rgb(97, 96, 95);
+  }
+</style>
